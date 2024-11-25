05559127081366f292eacdc9e3721b9419e85263
clod2008 <claudio@apsis.com.ar>
2024-11-25 18:24:17 -0300

    clg

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ document.getElementById('fetchButton').addEventListener('click', async () => {
         const response = await fetch(`/fetch-url?url=${encodeURIComponent(url)}`);
         const data = await response.json();
         resultElement.textContent = JSON.stringify(data, null, 2);
+        console.log(encodeURIComponent(url))
     } catch (error) {
         resultElement.textContent = `Error: ${error.message}`;
     }
